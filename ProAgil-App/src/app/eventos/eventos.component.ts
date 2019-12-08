ace539719f8b3e84f33abe019a2959d1af3291de
JBonfim <jabesbonfim@gmail.com>
2019-12-08 07:50:45 -0300

    adicionado filtro para o evento

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -10,16 +10,35 @@ import { HttpClient } from '@angular/common/http';
 
 export class EventosComponent implements OnInit {
 
+  _FILTROLISTA: string;
+
+  get filtroLista(): string {
+    return this._FILTROLISTA;
+  }
+  set filtroLista(value: string){
+    this._FILTROLISTA = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+
+  eventosFiltrados: any = [];
   eventos: any = [] ;
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImg = false;
-  filtroLista = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    console.log(filtrarPor);
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 
   mostrarImagem(){
